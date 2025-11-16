f09f61050531d718cbd001d7dbb4a524fb85fa0b
Piotr Kowalski <piotrkowalski2003@icloud.com>
2025-11-16 18:59:51 +0100

    19.3.Capture All Lists

---
+++ CppMasterclass/Section 19 - Lambda Functions/19.3.CaptureAllLists/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+
+int main(){
+
+    //Capture everything by value
+    
+     int c{42};
+     
+     auto func = [=](){
+         std::cout << "Inner value : " << c << std::endl;
+     };
+     
+     for(size_t i{} ; i < 5 ;++i){
+         std::cout << "Outer value : " << c << std::endl;
+         func();
+         ++c;
+     }
+     
+
+
+    //Capturing all reference
+    int c1{42};
+    int d{5};
+     
+    auto func1 = [&](){
+        std::cout << "Inner value : " << c1 << std::endl;
+        std::cout << "Inner value(d) : " << d << std::endl;
+    };
+     
+    for(size_t i{} ; i < 5 ;++i){
+        std::cout << "Outer value : " << c1 << std::endl;
+        func1();
+        ++c1;
+    }
+  
+    return 0;
+}
